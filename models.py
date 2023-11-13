c9884468fd8557da424d303bcb0cbb69eabf8866
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-11-12 22:48:05 -0500

    Added post comments table

--- models.py
+++ models.py
@@ -75,3 +75,42 @@ class Post(db.model):
 
     def __repr__(self) -> str:
         return f'Post #{self.post_id} by {self.user_id})'
+
+#post comments
+class Comment(db.model):
+    # PRIMARY KEY comment_id SERIAL,
+    comment_id = db.Column(db.Integer, primary_key = True)
+
+    # content TEXT,
+    content = db.Column(db.Text, nullable = True)
+
+    # date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_posted = db.Column(db.DateTime, nullable = False)
+
+    # likes INT NOT NULL DEFAULT 0,
+    likes = db.Column(db.Integer, nullable = True, default = 0)
+
+    # user_id INT,
+    # FOREIGN KEY (user_id) references users(user_id)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
+
+    # post_id INT,
+    # FOREIGN KEY (post_id) references posts(post_id)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable = False)
+
+    # each user's comments
+    comments = db.relationship('users', backref='comments')
+
+    # each post's comments
+    comments = db.relationship('posts', backref='comments')
+
+    def __init__(self, comment_id: int, user_id: int, post_id: int, content: str, date_posted: str, likes: int):
+        self.comment_id = comment_id
+        self.user_id = user_id
+        self.post_id = post_id
+        self.content = content
+        self.date_posted = date_posted
+        self.likes = likes
+
+    def __repr__(self) -> str:
+        return f'{self.content}'
