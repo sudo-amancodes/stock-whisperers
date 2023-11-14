fc443d62fbf219a09fb99b1bb4c05dc3460fa5e9
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-11-14 18:34:20 -0500

    Correctly modeled db for posts and comments

--- src/models.py
+++ src/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 db = SQLAlchemy()
 
 #users table
-class users():
+class users(db.Model):
     user_id = db.Column(db.Integer, primary_key = True)
     # first_name VARCHAR(255),
     first_name = db.Column(db.String(255), nullable = False)
@@ -39,7 +40,7 @@ class Live_Posts():
 
 
 #post
-class Post(db.model):
+class Post(db.Model):
     # PRIMARY KEY post_id SERIAL,
     post_id = db.Column(db.Integer, primary_key = True)
 
@@ -50,13 +51,13 @@ class Post(db.model):
     content = db.Column(db.Text, nullable = True)
 
     # date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    date_posted = db.Column(db.DateTime, nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, server_default=text('NOW()'))
 
     # likes INT NOT NULL DEFAULT 0,
     likes = db.Column(db.Integer, nullable = True, default = 0)
 
     # image BLOB,
-    image = db.Column(db.LargeBinary, nullable = True)
+    file_upload = db.Column(db.String(255), nullable = True, default = 'default.jpg')
 
     # user_id INT,
     # FOREIGN KEY (user_id) references users(user_id)
@@ -65,19 +66,20 @@ class Post(db.model):
     # each user's posts
     posts = db.relationship('users', backref='posts')
 
-    def __init__(self, post_id: int, user_id: int, title: str, content: str, date_posted: str, likes: int):
-        self.post_id = post_id
-        self.user_id = user_id
+    def __init__(self, title: str, content: str):
         self.title = title
         self.content = content
-        self.date_posted = date_posted
-        self.likes = likes
+
+    # def __init__(self, user_id: int, title: str, content: str):
+    #     self.user_id = user_id
+    #     self.title = title
+    #     self.content = content
 
     def __repr__(self) -> str:
         return f'Post #{self.post_id} by {self.user_id})'
 
 #post comments
-class Comment(db.model):
+class Comment(db.Model):
     # PRIMARY KEY comment_id SERIAL,
     comment_id = db.Column(db.Integer, primary_key = True)
 
@@ -85,7 +87,7 @@ class Comment(db.model):
     content = db.Column(db.Text, nullable = True)
 
     # date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    date_posted = db.Column(db.DateTime, nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, server_default=text('NOW()'))
 
     # likes INT NOT NULL DEFAULT 0,
     likes = db.Column(db.Integer, nullable = True, default = 0)
@@ -96,21 +98,18 @@ class Comment(db.model):
 
     # post_id INT,
     # FOREIGN KEY (post_id) references posts(post_id)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable = False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable = False)
 
     # each user's comments
-    comments = db.relationship('users', backref='comments')
+    user_comments = db.relationship('users', backref='user_comments', foreign_keys=[user_id])
 
     # each post's comments
-    comments = db.relationship('posts', backref='comments')
+    post_comments = db.relationship('Post', backref='post_comments', foreign_keys=[post_id])
 
-    def __init__(self, comment_id: int, user_id: int, post_id: int, content: str, date_posted: str, likes: int):
-        self.comment_id = comment_id
+    def __init__(self, user_id: int, post_id: int, content: str):
         self.user_id = user_id
         self.post_id = post_id
         self.content = content
-        self.date_posted = date_posted
-        self.likes = likes
 
     def __repr__(self) -> str:
         return f'{self.content}'
