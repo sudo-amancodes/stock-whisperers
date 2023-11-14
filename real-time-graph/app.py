13ca8a9de96b9cc2c32ac61aefe058c711863b26
AmanW-GitHub <amanwaghchoure2004@gmail.com>
2023-11-14 18:01:28 -0500

    created real time graph

--- real-time-graph/app.py
+++ real-time-graph/app.py
@@ -8,41 +8,94 @@ import yfinance as yf
 #Graphing/Visualization
 import datetime as dt 
 import plotly.graph_objs as go 
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+import json
+import plotly
+import plotly.express as px
+import pandas as pd
+from threading import Lock
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+# async_mode = None
+
+thread = None
+thread_lock = Lock()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ABC'
+socketio = SocketIO(app, cors_allowed_origins='*')
+# socketio = SocketIO(app, async_mode=async_mode)
 
 #Override Yahoo Finance
 yf.pdr_override()
 
 #Create input field for our desired stock
-stock='AAPL'
+def get_plotly_json(stock):
+    #Retrieve stock data frame (df) from yfinance API at an interval of 1m
+    df = yf.download(tickers=stock,period='1d',interval='1m', threads = True)
+    df['Datetime'] = df.index.strftime('%I:%M %p')
 
-#Retrieve stock data frame (df) from yfinance API at an interval of 1m
-df = yf.download(tickers=stock,period='1d',interval='1m', threads = True)
-df['Datetime'] = df.index.strftime('%I:%M %p')
+    print(df)
+    #Declare plotly figure (go)
+    fig=go.Figure()
 
-print(df)
-#Declare plotly figure (go)
-fig=go.Figure()
+    fig.add_trace(go.Candlestick(x=df.index,
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close'], name = 'market data'))
 
-fig.add_trace(go.Candlestick(x=df.index,
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'], name = 'market data'))
+    fig.update_layout(
+        title= str(stock)+' Live Share Price:',
+        yaxis_title='Stock Price (USD per Shares)')
 
-fig.update_layout(
-    title= str(stock)+' Live Share Price:',
-    yaxis_title='Stock Price (USD per Shares)')
-
-fig.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=15, label="15m", step="minute", stepmode="backward"),
-            dict(count=45, label="45m", step="minute", stepmode="backward"),
-            dict(count=1, label="HTD", step="hour", stepmode="todate"),
-            dict(count=3, label="3h", step="hour", stepmode="backward"),
-            dict(step="all")
-        ])
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=15, label="15m", step="minute", stepmode="backward"),
+                dict(count=45, label="45m", step="minute", stepmode="backward"),
+                dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
     )
-)
-fig.show()
+    graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graph_json
+
+def background_thread():
+    print("Generating graph sensor values")
+    while True:
+        value = get_plotly_json('AAPL')  # Corrected stock symbol
+        socketio.emit('updateGraph', {'value': value})  # Corrected event name
+        socketio.sleep(10)
+
+@app.route('/')
+def index():
+    graph = get_plotly_json('AAPL')
+    return render_template('index.html', plot=graph)
+
+@socketio.on('connect')
+def connect():
+    global thread
+    print('Client connected')
+
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+
+"""
+Decorator for disconnect
+"""
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected',  request.sid)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
