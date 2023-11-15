fb4f7809bb57d9ae588230610d08ccd36b09158b
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-11-14 19:01:08 -0500

    Resolved conflict

--- src/models.py
+++ src/models.py
@@ -4,39 +4,57 @@ from sqlalchemy import text
 db = SQLAlchemy()
 
 #users table
-class users(db.Model):
+class users(db.Model, UserMixin):  
     user_id = db.Column(db.Integer, primary_key = True)
-    # first_name VARCHAR(255),
+
     first_name = db.Column(db.String(255), nullable = False)
-    # last_name VARCHAR(255),
+
     last_name = db.Column(db.String(255), nullable = False)
-    # username VARCHAR(255) UNIQUE,
+
     username = db.Column(db.String(255), nullable = False)
-    # email VARCHAR(255) UNIQUE,
+
     email = db.Column(db.String(255), nullable = False)
-    # password VARCHAR(255),
+
     password = db.Column(db.String(255), nullable = False)
-    # role VARCHAR(50),
-    # role = db.Column(db.String(255), nullable = False)
-    # registration_date TIMESTAMP,
-    # profile_picture VARCHAR(255)
+
+    role = db.Column(db.String(255), nullable = True)
+
+    registration_date = db.Column(db.DateTime, nullable=True, default=func.now())
+
     profile_picture = db.Column(db.String(255), nullable = True)
 
-    def __init__(self, user_id:int, first_name: str, last_name: str, username: str, email: str, password: str):
-        self.user_id = user_id
+    def __init__(self, first_name: str, last_name: str, username: str, password: str):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.email = email
+        # self.email = email
         self.password = password
 
     def __repr__(self) -> str:
         return f'users({self.first_name}, {self.last_name})'
+    
+    def get_id(self):
+        return str(self.user_id)
 
 
 #live posts table
-class Live_Posts():
-    pass
+class live_posts(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), nullable = True, default = func.now())
+    
+    # Foreign key referencing the users table
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    
+    # Establish the relationship with the User model
+    user = db.relationship('users', backref=db.backref('live_posts', lazy=True))
+
+    def __init__(self, content: str, user_id:int):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self) -> str:
+        return f'live_posts({self.post_id}, {self.content}, {self.user_id})'
 
 
 #post
